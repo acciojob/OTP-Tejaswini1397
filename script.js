375753e63403fcd9d0531412314923f0f9a70ea0
Tejaswini Khairnar <117677072+Tejaswini1397@users.noreply.github.com>
2023-12-06 12:11:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 //your JS code here. If required.
+let inp = document.querySelector("#code-1");
+window.onload(inp.focus());
+
+function focusNext(currentInput) {
+    const maxLength = parseInt(currentInput.getAttribute('maxlength'));
+    const nextInputId = parseInt(currentInput.id.split('-')[1]) + 1;
+
+    if (currentInput.value.length === maxLength && nextInputId <= 6) {
+        document.getElementById('code-' + nextInputId).focus();
+    }
+}
+
+function focusPrev(currentInput) {
+    const prevInputId = parseInt(currentInput.id.split('-')[1]) - 1;
+
+    if (event.key === 'Backspace' && prevInputId >= 1) {
+        currentInput.value = '';
+        document.getElementById('code-' + prevInputId).focus();
+    }
+}
