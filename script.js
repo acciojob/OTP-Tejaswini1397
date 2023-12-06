f980bb7375ff2010379ba8e18dd462cf0a4f2f67
Tejaswini Khairnar <117677072+Tejaswini1397@users.noreply.github.com>
2023-12-06 12:20:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,21 +1,25 @@
-//your JS code here. If required.
-let inp = document.querySelector("#code-1");
-window.onload(inp.focus());
+const codes = document.querySelectorAll(".code")
 
-function focusNext(currentInput) {
-    const maxLength = parseInt(currentInput.getAttribute('maxlength'));
-    const nextInputId = parseInt(currentInput.id.split('-')[1]) + 1;
+codes[0].focus()
 
-    if (currentInput.value.length === maxLength && nextInputId <= 6) {
-        document.getElementById('code-' + nextInputId).focus();
-    }
-}
+codes.forEach((code,idx)=>{
+    code.addEventListener("keydown",(e)=>{
+        console.log(e.key)
+        const keyPressed = e.key
 
-function focusPrev(currentInput) {
-    const prevInputId = parseInt(currentInput.id.split('-')[1]) - 1;
-
-    if (event.key === 'Backspace' && prevInputId >= 1) {
-        currentInput.value = '';
-        document.getElementById('code-' + prevInputId).focus();
-    }
-}
+        if(keyPressed>=0 && keyPressed<=9){
+            codes[idx].value=''
+            if(idx<codes.length-1){
+                setTimeout(()=>codes[idx+1].focus(),10)
+                // setTimeout(function(){
+                //     codes[index+1].focus()
+                // },10)
+            }
+        }
+        else if(keyPressed==="Backspace"){
+            if(idx>0){
+                setTimeout(()=>codes[idx-1].focus(),10)
+            }
+        }
+    })
+})
